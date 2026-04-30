11a57087e94b388008cfd8182b8d2bdf64ae4316
Jacob Swift <jacobvaughnswift@gmail.com>
2026-04-29 20:26:43 -0600

    Completed prepare Learning02 activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Electrical Engineer";
+        job1._company = "Raytheon";
+        job1._startYear = 2025;
+        job1._endYear = 2026;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Flex Manager";
+        job2._company = "Shepherd Univeristy";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jacob Swift";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
